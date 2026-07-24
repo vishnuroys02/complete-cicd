436ea36de9a36d924d155498f59c30bbc6a85ac9
vishnu <vishnuroys02@gmail.com>
2026-07-24 21:42:27 +0530

    Updated homepage

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to Complete CI/CD Pipeline"
+    return "Welcome to Complete CI/CD Pipeline - Version 2"
 
 @app.route("/health")
 def health():
