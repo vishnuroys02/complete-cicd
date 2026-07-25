868aa0657a6b62f2a81022de978ad441ad37f585
vishnuroys02 <vishnuroys02@gmail.com>
2026-07-25 07:59:33 +0530

    Update welcome message in home route

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to Complete CI/CD Pipeline - Version 2"
+    return "CI/CD Pipeline Version 2 - Successfully Deployed"
 
 @app.route("/health")
 def health():
